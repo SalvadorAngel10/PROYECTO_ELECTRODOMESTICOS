d44e25d53d5d3c5ea29ab2c7c0aef8323ab15444
34694 <saresaau@gmail.com>
2021-01-20 21:22:42 +0100

    Sesioan 1-Miercoles 20/1/2021-2 horas
    -Creación parte grafica del Mainscreen
    -Creacion de carpetas para almacenar las gainas ,clases,imagenes,xml
    -creacion del xml
    -En proceso de empezar las clases para  producto
    
    Nota:importante realizar mañana las paginas,sin problemas de momento.

--- PROYECTO_ELECTRODOMESTICOS/MainWindow.xaml.cs
+++ PROYECTO_ELECTRODOMESTICOS/MainWindow.xaml.cs
@@ -24,5 +24,35 @@ namespace PROYECTO_ELECTRODOMESTICOS
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
